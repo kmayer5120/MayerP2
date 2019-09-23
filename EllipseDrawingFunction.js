0ab77b425a9605b8329e9127dbda698e1c68d46f
Kyle Mayer <kmayer1@cnm.edu>
2019-09-23 11:56:05 -0600

    Added styling, linking, and more content.

---
+++ EllipseDrawingFunction.js
@@ -0,0 +1,33 @@
+/*
+*   Name:       Kyle Mayer
+*   File:       EllipseDrawingFunction.js
+*   Program:    Program 2
+*   Purpose:    Design a web page that informs user about the purpose of a website.
+*   Date:       9/10/2019
+*/
+
+
+//Source for function: http://www.williammalone.com/briefs/how-to-draw-ellipse-html5-canvas/
+function drawEllipse(centerX, centerY, width, height) {
+
+    var canvas = document.getElementById('ellipse');
+    var context = canvas.getContext('2d')
+
+    context.beginPath();
+
+    context.moveTo(centerX, centerY - height / 2); // A1
+
+    context.bezierCurveTo(
+        centerX + width / 2, centerY - height / 2, // C1
+        centerX + width / 2, centerY + height / 2, // C2
+        centerX, centerY + height / 2); // A2
+
+    context.bezierCurveTo(
+        centerX - width / 2, centerY + height / 2, // C3
+        centerX - width / 2, centerY - height / 2, // C4
+        centerX, centerY - height / 2); // A1
+
+    context.fillStyle = "red";
+    context.fill();
+    context.closePath();
+}
